fad585770727bf284d8e5dc6cab35d3214fb87d9
alenk89 <77924959+alenk89@users.noreply.github.com>
2024-06-13 01:09:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -764,21 +764,16 @@ function append_files_to_list(path, files) {
 
       html +=
         `
-           <a class="countitems size_items list-group-item-action" style="text-decoration: none;" href="javascript:void(0)" onclick="myFunction()">${item.name}</a>
+           <a class="countitems size_items list-group-item-action" style="text-decoration: none;" href="${link}">${item.name}</a>
            ${UI.display_size ? `<span class="badge badge-dim bg-dark p-1x float-end"> ` + item['size'] + ` </span>`
           : ``}
            ${UI.display_time ? ` <span class="badge badge-dim bg-dark p-1x float-end"> ` + item['modifiedTime'] + ` </span>` : ``}
            ${UI.display_download ?
           `
-              <a href="javascript:void(0)" onclick="myFunction()" class="btn btn-dim btn-primary p-1x">
+              <a href="${link}" class="btn btn-dim btn-primary p-1x">
               <i class="ri-download-2-line fs-4 me-1"></i>
               <span>Download</span>
               </a>
-              <script>
-                  function myFunction() {
-                    location.replace("${link}")
-                  }
-              </script>
            `
           : ``}
            
